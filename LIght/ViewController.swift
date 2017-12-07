35e5a40aa0e5d0e2e0420e64eb81bcfb2b75cbd5
student <student@mail.sbisoft.ru>
2017-12-07 13:50:57 +0300

    commit2

--- LIght/ViewController.swift
+++ LIght/ViewController.swift
@@ -10,18 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var isLightOn = true
     @IBAction func buttonPressed(_ sender: Any) {
-        view.backgroundColor = UIColor.black
+        isLightOn = !isLightOn
+        updateView()
+    }
+    func updateView(){
+        if isLightOn {
+            view.backgroundColor = UIColor.white
+        }
+        else
+        {
+            view.backgroundColor = UIColor.black
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+
 
 
 }
